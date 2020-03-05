349432ae7b6584233eab3c5a6bf6f22d18e557c1
Joesujin <joesujin@gmail.com>
2020-03-05 16:40:56 +0530

    altered

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,17 @@
 var canvas;
 
-function setup(){
-    canvas = createCanvas(windowWidth,windowHeight);
-    canvas.position(0,0);
-    canvas.style('z-index','-1');
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
 }
 
-function draw(){
+function draw() {
+    fill(0);
+    noStroke();
+    rect(0, 0, width * 2, height * 2);
+
+
     noFill();
     strokeWeight(1);
     stroke(random(255));
