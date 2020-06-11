efe976f2b53497335dea00fcaafbbcb14fb368cb
Joesujin <joesujin@gmail.com>
2020-06-12 02:52:32 +0530

    Added New Sketch

--- js/grid_layout.js
+++ js/grid_layout.js
@@ -1,9 +1,12 @@
 var stepX;
 var tileCount;
 var stepY;
-var actRandomSeed =0;
+var actRandomSeed = 0;
+var ranNeg;
+var ranPos;
 
-
+var xWidth;
+var yHeight;
 var canvas;
 
 function setup() {
@@ -11,44 +14,87 @@ function setup() {
     canvas.position(0, 0);
     canvas.class('joe-bg');
     background(255);
-    colorMode(HSB, width, height, 100);
-    tileCount = 100;
+    //colorMode(HSB, width, height, 100);
+    tileCount = 50;
     //frameRate(1);
+    ranNeg =random(-5,0);
+    ranPos =random(0,5);
+    xWidth = width/tileCount;
+    yHeight = height/tileCount;
 }
 
 function draw() {
-    background(255);
+    background(18, 15, 38);
     //translate(width/2, height/2);
+    noFill();
+    stroke(10);
 
     randomSeed(actRandomSeed);
-    for(var gridY=0; gridY< tileCount; gridY++){
-        for(var gridX=0; gridX< tileCount; gridX++){
-            var posX = (width/tileCount)*gridX;
-            var posY = (height/tileCount)*gridY;
-            
-            var toggle= int(random(0,2));
+    for (var gridY = 0; gridY < tileCount; gridY++) {
+        for (var gridX = 0; gridX < tileCount; gridX++) {
+            var posX = (xWidth) * gridX;
+            var posY = (yHeight) * gridY;
+            strokeWeight(10);
+            var toggle = int(random(0, 4));
 
-            if(toggle ==0){
-                strokeWeight(mouseX/100);
-                if((mouseX-posX)==0){
-                    stroke(200,mouseX,200);
-                }
-                else{
-                    stroke(0);
-                }
-                line(posX, posY, posX + width/tileCount, posY + height/tileCount);
+            if (toggle == 0) {
+
+                stroke( 20, 151, 166);
+                push();
+                translate(posX, posY);
+                rotate(map(mouseX,0,width,ranNeg,ranPos))
+
+                
+                arc(0, 0, xWidth, yHeight, 0, HALF_PI);
+                arc(xWidth, yHeight, xWidth, yHeight, PI, PI+HALF_PI);
+                pop();
+
             }
-            if(toggle ==1){
-                strokeWeight(mouseY/100);
-                line(posX, posY + width/tileCount, posX + height/tileCount, posY);
+            if (toggle == 1) {
+                stroke(121, 217, 199);
+                push();
+                translate(posX+width/tileCount,posY);
+                rotate(map(mouseX,0,width,ranNeg,ranPos));
+                arc(0, 0, xWidth, yHeight,  HALF_PI,PI);
+                arc(-xWidth, yHeight, xWidth, yHeight,PI+HALF_PI,TWO_PI);
+
+                pop();
+                
             }
+            if (toggle == 2) {
+                stroke(242, 221, 213);
+                push();
+                translate(posX+width/tileCount, posY+ height/tileCount);
+                rotate(map(mouseX,0,width,ranNeg,ranPos));
+                arc(0, 0, xWidth, yHeight, PI,PI+HALF_PI);
+                arc(-xWidth, -yHeight, xWidth, yHeight, TWO_PI, TWO_PI+HALF_PI);
+                
+                pop();
+                
+            }
+            if (toggle == 3) {
+                stroke(217, 59, 59);
+                push();
+                translate(posX, posY+ height/tileCount);
+                rotate(map(mouseX,0,width,ranNeg,ranPos));
+                arc(0, 0, xWidth, yHeight, PI+HALF_PI,TWO_PI);
+                arc(xWidth, -yHeight, xWidth, yHeight, HALF_PI,PI);
+                
+                pop();
+                
+            }
+            /*
+            */
         }
     }
-    
-    
-
+    console.log(map(mouseX,0,width,-2,2),map(mouseY,0,height,-2,2))
 }
 
-function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
+
+
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    xWidth = width/tileCount;
+    yHeight = height/tileCount;
 }
--- js/spirograph_2.js
+++ js/spirograph_2.js
@@ -12,16 +12,17 @@ function setup() {
     scl = abs(width-height)/32;
     val =-10;
     smooth(12)
-    colorMode(HSB,360,100,100,100);
+    //colorMode(HSB,250,100,100,100);
 }
+
 
 function draw() {
     //background(0);
-    fill(0,5);
+    fill(0,45);
     noStroke();
     rect(0,0,width,height);
-
-    stroke(255);
+    stroke(0);
+    //stroke(1, 58, 64);
     strokeWeight(2);
 
     scl =50;
@@ -34,14 +35,36 @@ function draw() {
     let b=mapval2;
     push();
     translate(width/2,height/2);
-    for(let i=0; i<=100; i++){
-        //stroke(i);
-        strokeWeight(i/16);
+    beginShape(TRIANGLES);
+    for(let i=0; i<=500; i++){
+        //stroke(i/2,80,60);
+        //colorMode(RGB);
+        if((i%4)==0){
+            fill(3, 140, 127,100);
+        }
+        if((i%4)==1){
+            fill( 4, 191, 138,100);
+        }
+        if((i%4)==2){
+            fill( 125, 191, 59,100);
+        }
+        if((i%4)==4){
+            fill(159, 242, 34,100);
+        }
+        if((i%4)==3){
+            fill(1, 58, 64,100);
+        }
+        
+        //fill(i/3,80,60,30);
+        //  fill(0,60);
+        //strokeWeight(i/512);
 
         //line(x1(val+i),y1(val+i),x2(val+i),y2(val+i));
-        line(x1(val+i,a,b),y1(val+i,a,b),x1(val+i-5,a,b),y1(val+i-5,a,b));
-        // point(x1(val+i,a,b),y1(val+i,a,b));
+        // line(x1(val+i,a,b),y1(val+i,a,b),x1(val+i-5,a,b),y1(val+i-5,a,b));
+        // point(x1(val+i+200,a,b),y1(val+i+200,a,b));
+        vertex(x1(val+i+200,a,b),y1(val+i+200,a,b));
     }
+    endShape();
     pop();
 
     val+=0.01;    
@@ -66,6 +89,7 @@ function y2(t, a, b) {
     let v2 = ((a - b) * sin(t)) + (b * sin(((a - b) / b) * t));
     return v2 * scl;
 }
+
 
 
 
